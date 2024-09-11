a12fcebb0263abeff71452462ef75f6e37f963f9
Anuj Kumar <99342265+AnujIstwal@users.noreply.github.com>
2024-09-11 17:10:57 +0530

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,50 +4,55 @@ import ProductItem from "./ProductItem";
 
 export default function Product({ productToShow }) {
     const { product, rating, sort } = productToShow;
-    let productList = productDetails;
+    let productList1 = productDetails;
 
     //product filter
     if (product === "headphone") {
-        productList = productList.filter(
+        productList1 = productList1.filter(
             (product) => product.category === "headphone"
         );
     } else if (product === "earphone") {
-        productList = productList.filter(
+        productList1 = productList1.filter(
             (product) => product.category === "earphone"
         );
     } else if (product === "speaker") {
-        productList = productList.filter(
+        productList1 = productList1.filter(
             (product) => product.category === "speaker"
         );
     } else if (product === "watch") {
-        productList = productList.filter(
+        productList1 = productList1.filter(
             (product) => product.category === "watch"
         );
     } else {
-        productList = productDetails;
+        productList1 = productDetails;
     }
+
+    let productList2 = productList1;
 
     //rating filter
     if (rating === ">4") {
-        productList = productList.filter((product) => product.rating > 4);
+        productList2 = productList2.filter((product) => product.rating > 4);
     } else if (rating === ">3") {
-        productList = productList.filter((product) => product.rating > 3);
+        productList2 = productList2.filter((product) => product.rating > 3);
     } else {
-        productList = productDetails;
+        productList2 = productList1;
     }
 
+    let productList3 = productList2;
     //sort
     if (sort === "review") {
-        productList = productList.sort((a, b) => b.reviews - a.reviews);
+        productList3 = productList3.sort((a, b) => b.reviews - a.reviews);
     } else if (sort === "price") {
-        productList = productList.sort((a, b) => a.price - b.price);
-    } else if (sort === "name") {
-        productList = productList.sort((a, b) => a.name.localeCompare(b.name));
+        productList3 = productList3.sort((a, b) => a.price - b.price);
+    } else {
+        productList3 = productList2.sort((a, b) =>
+            a.name.localeCompare(b.name)
+        );
     }
 
     return (
         <div className="products-container bg-white mx-auto max-w-4xl p-6 rounded-xl">
-            {productList.map((product) => (
+            {productList3.map((product) => (
                 <ProductItem key={product.id} product={product} />
             ))}
         </div>
